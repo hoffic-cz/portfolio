4dd14eb7e95fd9a61e3160e13ad508b45d233166
Petr Hoffmann <petr.hoffic@gmail.com>
2020-01-16 19:27:59 +0100

    Set up webpack.

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function __invoke()
+    {
+        return $this->render('pages/index.html.twig');
+    }
+}
