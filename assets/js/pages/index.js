7c30c8c8a93c2b87a21dd4e60e9e3ea1c3b7c359
Petr Hoffmann <petr.hoffic@gmail.com>
2020-01-17 14:05:42 +0100

    Enabled prompt input.

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,21 +1,6 @@
-import {Terminal} from "xterm";
-import {FitAddon} from "xterm-addon-fit";
 import $ from "jquery";
+import {setUpTerminal} from "../partial/terminal";
 
 $(document).ready(function () {
-  const terminal = new Terminal({
-    theme: {
-      background: '#333333', // in colours.scss as $color-terminal-content
-    }
-  });
-  const fitAddon = new FitAddon();
-
-  terminal.loadAddon(fitAddon);
-  terminal.open(document.getElementById('xterm'));
-  fitAddon.fit();
-
-  terminal.writeln('Hello!');
-  for (let i = 0; i < 100; i++) {
-    terminal.writeln('Yo');
-  }
+  setUpTerminal(document.getElementById('xterm'));
 });
---
+++ assets/js/partial/terminal.js
@@ -0,0 +1,59 @@
+import {Terminal} from "xterm";
+import {FitAddon} from "xterm-addon-fit";
+
+const promptText = 'visitor@hoffic.dev:~$ ';
+
+export function setUpTerminal(terminalElement) {
+  const terminal = new Terminal({
+    theme: {
+      background: '#333333', // in colours.scss as $color-terminal-content
+    }
+  });
+  const fitAddon = new FitAddon();
+
+  terminal.loadAddon(fitAddon);
+  terminal.open(terminalElement);
+  fitAddon.fit();
+
+  configureInput(terminal);
+
+  terminal.prompt();
+}
+
+function configureInput(terminal) {
+  terminal.onKey(function (event) {
+    switch (event.key.charCodeAt(0)) {
+      case 13:
+        keyEnter(terminal, event.key);
+        break;
+      case 127:
+        keyBackspace(terminal, event.key);
+        break;
+      default:
+        keyOther(terminal, event.key);
+        break;
+    }
+
+    console.log(event);
+  });
+
+  terminal.prompt = function () {
+    terminal.write(promptText);
+  }
+}
+
+function keyEnter(terminal, key) {
+  terminal.write('\n');
+  terminal.write(key);
+  terminal.prompt();
+}
+
+function keyBackspace(terminal) {
+  if (terminal._core.buffer.x > promptText.length) {
+    terminal.write('\b \b');
+  }
+}
+
+function keyOther(terminal, key) {
+  terminal.write(key);
+}
