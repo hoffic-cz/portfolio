8360439672c5d0a5ebf438cfcf63a8361e20c016
Petr Hoffmann <petr.hoffic@gmail.com>
2020-01-18 15:11:47 +0100

    Added tests for CommandController.

---
+++ tests/Controller/CommandControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CommandControllerTest extends WebTestCase
+{
+    public function testResponseFormat()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/command/',
+            [],
+            [],
+            [],
+            '{"command":"intro"}'
+        );
+
+        $this->assertTrue(is_string(json_decode($client->getResponse()->getContent())->stdout));
+    }
+
+    public function testGarbledInput()
+    {
+        $client = static::createClient();
+        $client->request(
+            'POST',
+            '/command/',
+            [],
+            [],
+            [],
+            '}Hello Clem!{'
+        );
+
+        self::assertEquals(400, $client->getResponse()->getStatusCode());
+    }
+}
