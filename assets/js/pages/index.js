d6349b050b153b20d846f5913b3a47c9a4b6159f
Petr Hoffmann <petr.hoffic@gmail.com>
2020-01-28 14:38:34 +0000

    Added a console note.

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,8 +1,10 @@
 import $ from "jquery";
 import {setUpTerminal} from "../partial/terminal";
 import {hookFooterUp} from "../partial/footer";
+import {layConsoleEgg} from "../partial/console-egg";
 
 $(document).ready(function () {
   setUpTerminal(document.getElementById('xterm'));
   hookFooterUp();
+  layConsoleEgg();
 });
---
+++ assets/js/partial/console-egg.js
@@ -0,0 +1,21 @@
+export function layConsoleEgg() {
+  let message = `
+  _    _ _ _ 
+ | |  | (_) |
+ | |__| |_| |
+ |  __  | | |
+ | |  | | |_|
+ |_|  |_|_(_)
+
+
+ Are you looking for JS errors?
+
+ Have you tried viewing the page source?
+
+ You can probably tell I do back end when
+ I made my portfolio a command line app :D
+ 
+ `;
+
+  console.log(message);
+}
